b97fa321ab956ef723cb9d5f52bf311cff60f624
Ryan Hopkins <Ryan.Hopkins@snhu.edu>
2019-08-18 12:13:24 -0400

    Add it145 conversion

---
+++ IT145ConversionToCSharp/IT145ConversionToCSharp/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
+namespace IT145ConversionToCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool logoff = false;
+            string userLog;
+            int failedAttempts = 0;
+
+            
+            do
+            {
+                Console.WriteLine("Type \"Login\" or \"Logoff\" to continue: ");
+                userLog = Console.ReadLine();
+
+                if (userLog.ToLower().Equals("login"))
+                {
+                    // Get username
+                    Console.Write("Enter username: ");
+                    string username = Console.ReadLine();
+
+                    // Get password
+                    Console.Write("Enter password: ");
+                    string password = Console.ReadLine();
+
+                    // Get role
+                    VerifyUser user = new VerifyUser();
+                    string role = user.verifyUserAccess(username, password);
+
+                    // Determine if role is valid or not
+                    if (role.Equals(""))
+                    {
+                        Console.WriteLine("Username and password returned no user permissions.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n" + user.getUserRoleFile(role) + "\n");
+                        continue;
+                    }
+                }
+
+                // User logs off directly
+                if (userLog.ToLower().Equals("logoff"))
+                {
+                    logoff = true;
+                    break;
+                }
+                else
+                {
+                    // User failed a login attempt
+                    Console.WriteLine("Invalid input, please try again.");
+                    failedAttempts++;
+                }
+
+                // Close program if user fails three times
+                if (failedAttempts >= 3)
+                {
+                    Console.WriteLine("Maximum number of failed login attempts reached. Program closing.");
+                    logoff = true;
+                    continue;
+                }
+
+
+            } while (!logoff);
+
+
+
+        }
+    }
+}
---
+++ IT145ConversionToCSharp/IT145ConversionToCSharp/VerifyUser.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace IT145ConversionToCSharp
+{
+    public class VerifyUser
+    {
+        private string hashedPassword;
+
+        public string verifyUserAccess(string username, string password)
+        {
+            StreamReader sr = new StreamReader(@"Files/credentials.txt");
+
+            // Create MD5 hash for password
+            MD5 md5hash = MD5.Create();
+            MD5Hash hash = new MD5Hash();
+            hashedPassword = hash.GetMd5Hash(md5hash, password);
+
+            // Continue until end of credential file
+            while (!sr.EndOfStream)
+            {
+                var nextLine = sr.ReadLine();
+
+                // Split Streamreader line by tabs
+                string[] userInfo = nextLine.Split("\t");
+
+                // Remove quotation marks from password string
+                userInfo[2] = userInfo[2].Trim('"');
+
+                // Hard coded array positons for added security, credential file must be updated to exact
+                // for credentials to be valid
+                if (username == userInfo[0] && hashedPassword == userInfo[1] && password == userInfo[2])
+                {
+                    return userInfo[3];
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return "";
+        }
+
+        public string getUserRoleFile(string role)
+        {
+            // Return blank string if role is not valid
+            if (role == "")
+            {
+                return "";
+            }
+
+            return System.IO.File.ReadAllText(@"Files/" + role + ".txt");
+            
+        }
+
+    }
+}
